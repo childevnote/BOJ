eda2a9edbafb9856a4e86730ad6506b693c111ef
childevnote <daniel011125@gmail.com>
2023-11-19 23:41:21 +0900

    solve : 2720

--- [BOJ] 2720번_세탁소 사장 동혁/solution.py
+++ [BOJ] 2720번_세탁소 사장 동혁/solution.py
@@ -1,6 +1,13 @@
-# 11:18
+# 11:18 ~ 11:35
+def solution():
+    t = int(input())
+    for i in range(t):
+        tc = int(input())
+        q = tc//25
+        d = (tc%25)//10
+        n = ((tc%25)%10)//5
+        p = tc%5
+        print(f"{q} {d} {n} {p}")
 
-t = int(input())
-tc = [int(input()) for _ in range(t)]
 
-print(t,tc)
+solution()
