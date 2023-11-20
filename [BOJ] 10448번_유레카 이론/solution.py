94d279cda7bd63b2e0d7d8d8e6a0fcf1a451e764
childevnote <daniel011125@gmail.com>
2023-11-20 16:03:28 +0900

    solve : 10448

---
+++ [BOJ] 10448번_유레카 이론/solution.py
@@ -0,0 +1,17 @@
+# 11:10 ~ 13:30
+def solution():
+    n = int(input())
+    tc = [int(input()) for _ in range(n)]
+    triangular = [int(i * (i + 1) / 2) for i in range(1, 45)]
+
+    for i in range(len(tc)):
+        eureka = 0
+        for j in range(len(triangular)):
+            for k in range(j, len(triangular)):
+                for l in range(k, len(triangular)):
+                    if triangular[j]+triangular[k]+triangular[l] == tc[i]:
+                        eureka = 1
+        print(eureka)
+
+
+solution()
