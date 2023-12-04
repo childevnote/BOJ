9fac88ac2333ed02bc20a8ec7061d5c5e9bc0fd1
childevnote <daniel011125@gmail.com>
2023-12-04 23:49:26 +0900

    tried : 4796

--- [BOJ] 4796번_캠핑/solution.py
+++ [BOJ] 4796번_캠핑/solution.py
@@ -1,13 +1,25 @@
 import sys
 
 read = sys.stdin.readline
+
+
 def solution():
-    tc = []
+    index = 1
+    answer = []
     while True:
-        num = [list(map(int, read().split()))]
-        if num[0] == 0:
-            break
+        num = list(map(int, read().split()))
+        if num[0] != 0:
+            answer.append((index, num))
+
         else:
-            tc.append(num)
-    print(tc)
+            break
+        index += 1
+
+    for index, num in answer:
+        if num[2] % num[1] < num[0]:
+            print(f"Case {index}: {(num[2] // num[1]) * num[0] + num[2] % num[1]}")
+        else:
+            print(f"Case {index}: {((num[2] // num[1]) + num[0]) * num[0]}")
+
+
 solution()
