708cc53a28612156a3d0a42a972b8b003ac956a9
childevnote <daniel011125@gmail.com>
2023-12-11 22:45:51 +0900

    solved : 1764

--- [BOJ] 1764번_듣보잡/solution.py
+++ [BOJ] 1764번_듣보잡/solution.py
@@ -1,8 +1,22 @@
+# 22:04 ~ 22:24
 import sys
 
 read = sys.stdin.readline
+write = sys.stdout.writelines
+
 
 def solution():
+    n, m = map(int, read().split())
+    listen = set(read() for _ in range(n))
+    answer = []
+    count = 0
+    for i in range(m):
+        look = read()
+        if look in listen:
+            count += 1
+            answer.append(look)
+    print(count)
+    write(sorted(answer))
 
 
 solution()
